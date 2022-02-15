3afcc4b1d706eb1dfc9e167bff807590f46a0248
Sebastian Röse <sebastian.roese@gmail.com>
2022-02-15 21:55:38 +0100

    Initial commit for marille fruit, most and marille-hot

---
+++ item.lua
@@ -0,0 +1,53 @@
+  data:extend({
+    {
+        icon = "__base__/graphics/icons/battery.png",
+        icon_mipmaps = 4,
+        icon_size = 64,
+        name = "marille-fruit",
+        order = "xyz",
+        stack_size = 200,
+        subgroup = "raw-material",
+        type = "item"
+    },
+    {
+        base_color = {
+          b = 0.03,
+          g = 0.32000000000000002,
+          r = 0.15
+        },
+        default_temperature = 18,
+        flow_color = {
+          b = 0.31000000000000001,
+          g = 0.75,
+          r = 0.42999999999999998
+        },
+        heat_capacity = "0.1KJ",
+        icon = "__base__/graphics/icons/fluid/lubricant.png",
+        icon_mipmaps = 4,
+        icon_size = 64,
+        name = "marille-most",
+        order = "xyz",
+        type = "fluid"
+      },
+      {
+        base_color = {
+          b = 0.03,
+          g = 0.32000000000000002,
+          r = 0.15
+        },
+        default_temperature = 70,
+        flow_color = {
+          b = 0.31000000000000001,
+          g = 0.75,
+          r = 0.42999999999999998
+        },
+        heat_capacity = "0.1KJ",
+        icon = "__base__/graphics/icons/fluid/lubricant.png",
+        icon_mipmaps = 4,
+        icon_size = 64,
+        name = "marille-hot",
+        order = "xyz",
+        type = "fluid"
+      },
+        
+  })
