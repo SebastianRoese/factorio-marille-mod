235659f5819e92b4b3900de856f3854ceaa65b97
Christian Bollmeyer <ChristianBollmeyer@gmail.com>
2022-02-22 21:47:15 +0100

    Add new items (flask, marille-aged, marille-pure)

--- item.lua
+++ item.lua
@@ -10,6 +10,26 @@
         type = "item"
     },
     {
+        icon = "__base__/graphics/icons/battery.png",
+        icon_mipmaps = 4,
+        icon_size = 64,
+        name = "flask",
+        order = "xyz",
+        stack_size = 200,
+        subgroup = "tool",
+        type = "item"
+  },
+  {
+        icon = "__base__/graphics/icons/battery.png",
+        icon_mipmaps = 4,
+        icon_size = 64,
+        name = "marille-aged",
+        order = "xyz",
+        stack_size = 200,
+        subgroup = "intermediate-product",
+        type = "item"
+  },
+  {
         base_color = {
           b = 0.03,
           g = 0.32000000000000002,
@@ -49,5 +69,24 @@
         order = "xyz",
         type = "fluid"
       },
-        
+      {
+        base_color = {
+          b = 0.03,
+          g = 0.32000000000000002,
+          r = 0.15
+        },
+        default_temperature = 18,
+        flow_color = {
+          b = 0.31000000000000001,
+          g = 0.75,
+          r = 0.42999999999999998
+        },
+        heat_capacity = "0.1KJ",
+        icon = "__base__/graphics/icons/fluid/lubricant.png",
+        icon_mipmaps = 4,
+        icon_size = 64,
+        name = "marille-pure",
+        order = "xyz",
+        type = "fluid"
+      }
   })
